527a0d1eb42223721a3e5846e2447bcea0c2c9ac
4yn <changsiyuan1999@gmail.com>
2022-01-08 06:11:49 +0800

    expose for react proxy

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,23 +7,23 @@ import mockData from "./mocks"
 
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:3001",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// const corsOptions = {
+//   origin: "http://localhost:3001",
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 const store = [...mockData];
 
-app.get("/data", (req, res) => {
+app.get("/api/data", (req, res) => {
   res.json(store);
 });
 
-app.post("/data", (req, res) => {
+app.post("/api/data", (req, res) => {
   console.log(req, req.body);
   const body: CellData = req.body;
   store.push(body);
